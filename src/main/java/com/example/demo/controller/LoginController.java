3a3d9f1f7c5dac0215725f3a8fd73914af173c3a
Dggrdg <kobe640670@gmail.com>
2025-01-16 17:43:47 +0800

    20250116 更新01

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.User;
@@ -14,7 +15,7 @@ public class LoginController {
 	private LoginService loginService;
 
 	@PostMapping("/login")
-	public User Login(User user) {
+	public User Login(@RequestBody User user) {
 		return loginService.login(user);
 	}
 }
